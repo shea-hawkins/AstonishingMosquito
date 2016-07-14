d72aeaa888b5e5a0ac49b60f27543d2daaf5f813
Shea Hawkins <Shea Hawkins>
2016-07-14 15:10:37 -0700

    Add index.js to application

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    entry: "./app/App.js",
+    entry: "./app/index.js",
     output: {
         path: __dirname,
-        filename: "bundle.js"
+        filename: "build/bundle.js"
     },
     module: {
         loaders: [

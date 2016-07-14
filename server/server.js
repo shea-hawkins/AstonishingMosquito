f1142b4f4c330ec45fb9c451711026a3049f42e4
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-14 16:53:06 -0700

    Set up initial library routing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan'); 
 var path = require('path');
+var libraryRoute = require('./resources/library/LibraryRoute');
 
 var app = express(); 
 
@@ -8,6 +9,7 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(path.join(__dirname + '/../client/assets')));
 app.use('/build', express.static(path.join(__dirname + '/../client/build')));
+app.use('/library', libraryRoute);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/../client/index.html'));

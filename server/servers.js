abdc54b2510a3618df2c984512c0d6c2d42657e4
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-14 15:47:18 -0700

    Set up express server and serve assets/build folders

--- server/servers.js
+++ server/servers.js
@@ -1,3 +1,17 @@
 var express = require('express');
 var app = express(); 
+var morgan = require('morgan'); 
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/build', express.static(__dirname + '/build'));
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.listen(port, function() {
+  console.log('Magic happening on port', port);
+});
 

9ada9888bca963c8e949c982bcac36fdb36f769a
Shea Hawkins <Shea Hawkins>
2016-07-14 17:20:39 -0700

    Add Router to the application

--- client/app/App.js
+++ client/app/App.js
@@ -4,20 +4,12 @@ import HeaderView from './resources/stateless/HeaderView';
 import LibraryView from './resources/libraryView/LibraryView';
 import GameView from './resources/gameView/GameView';
 
-
-var exampleSongs = [
-  {id: 1, title: 'French Letter'},
-  {id: 2, title: 'Born Ready'},
-  {id: 3, title: 'Indian Summer'}
-];
-
 class App extends React.Component {
   render() {
     return (
       <div>
         <HeaderView />
-        <GameView />
-        <LibraryView songs={exampleSongs} />
+        {this.props.children}
       </div>
     )
   }
--- client/app/index.js
+++ client/app/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+
 import Store from './Store.js';
 import App from './App.js';
-
+import GameView from './resources/gameView/GameView';
+import LibraryView from './resources/libraryView/LibraryView';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path='game' component={GameView}/>
+        <Route path='library' component={LibraryView}/>
+        <Route path='*' component={LibraryView} />
+      </ Route>
+    </Router>
   </Provider>
   , document.getElementById('app'));

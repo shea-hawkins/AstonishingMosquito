99dfb65c8a192241671758145527c7e75bcc7b6a
Shea Hawkins <Shea Hawkins>
2016-07-14 17:23:57 -0700

    Connect LibraryView to redux store

--- client/app/Store.js
+++ client/app/Store.js
@@ -3,7 +3,9 @@ import { createStore } from 'redux';
 var actions = Object.assign({});
 
 var state = {
-
+  library: {
+    songs: [{id: 1, title: 'Song 1'}, {id: 2, title: 'Song 2'}, {id: 3, title: 'Song 3'}]
+  }
 };
 
 var reducer = function(prevState = state, action) {

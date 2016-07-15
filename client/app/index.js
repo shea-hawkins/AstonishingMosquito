b77f5469fe878e4737dc8022539ab79bb45b9570
Shea Hawkins <Shea Hawkins>
2016-07-14 18:46:25 -0700

    Add hashRouting

--- client/app/index.js
+++ client/app/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 
 import Store from './Store.js';
 import App from './App.js';
 import GameView from './resources/gameView/GameView';
 import LibraryView from './resources/libraryView/LibraryView';
 
+
+// TODO add default router
 ReactDOM.render(
   <Provider store={Store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path='/' component={App}>
+        <Route path='/' component={LibraryView} />
         <Route path='game' component={GameView}/>
         <Route path='library' component={LibraryView}/>
         <Route path='*' component={LibraryView} />

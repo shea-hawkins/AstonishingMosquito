1f24d0a57dfe0ea8f7b3ff4b8e6924a68ac667ed
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-14 20:29:55 -0700

    Return metadata successfully for songs in library

--- server/resources/library/LibraryRoute.js
+++ server/resources/library/LibraryRoute.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var fs = require('fs'); 
 var path = require('path');
+var mm = require('musicmetadata');
 
 var router = express.Router();
 
 router.get('/', function(req, res) {
   return new Promise(function(resolve, reject) {
     fs.readdir(path.join(__dirname + '/../../library'), 'utf8', function(err, files) {
-      err ? reject(err) : resolve(files); 
+      if (err) {
+        reject(err); 
+      } else {
+        files = files.filter(function(item) {
+          return !(/(^|\/)\.[^\/\.]/g).test(item);
+        });
+        resolve(files);
+      }
     });
   })
   .then(function(songFiles) {
-    res.send(songFiles);
+    var promises = [];
+    songFiles.forEach(function(song) {
+      promises.push(new Promise(function (resolve,reject) {
+        mm(fs.createReadStream(path.join(__dirname + '/../../library/' + song)), function (err, metadata) {
+          delete metadata.picture;
+          metadata.fileName = song;
+          resolve(metadata);
+        });
+      }));
+    });
+    return Promise.all(promises);
+  })
+  .then(function(metadata) {
+    res.send(metadata);
   });
 });
 

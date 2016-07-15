6690dc83195e2437dddc7dc5e4bb57c0b19e6f4f
Shea Hawkins <Shea Hawkins>
2016-07-15 16:13:10 -0700

    Store posted songs in the filesystem.

--- server/resources/library/LibraryRoute.js
+++ server/resources/library/LibraryRoute.js
@@ -1,7 +1,12 @@
 var express = require('express');
-var fs = require('fs'); 
+var fs = require('fs');
 var path = require('path');
 var mm = require('musicmetadata');
+
+var multer = require('multer');
+var upload = multer( {
+  dest: path.join(__dirname + '/../../library/') 
+} );
 
 var router = express.Router();
 
@@ -9,7 +14,7 @@ router.get('/', function(req, res) {
   return new Promise(function(resolve, reject) {
     fs.readdir(path.join(__dirname + '/../../library'), 'utf8', function(err, files) {
       if (err) {
-        reject(err); 
+        reject(err);
       } else {
         files = files.filter(function(item) {
           return !(/(^|\/)\.[^\/\.]/g).test(item);
@@ -34,6 +39,11 @@ router.get('/', function(req, res) {
   .then(function(metadata) {
     res.send(metadata);
   });
+});
+
+router.post('/', upload.single('song'), function(req, res) {
+  console.log(req.file);
+  res.end('sung');
 });
 
 module.exports = router;

a85fdbe9a67a024ef0054f92c7a195e9d110b4d5
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-07-15 18:23:13 -0700

    Implement entry into game view from librarylistview

--- client/app/resources/stateless/HeaderView.js
+++ client/app/resources/stateless/HeaderView.js
@@ -9,7 +9,6 @@ export default () => {
             <a href='#!' className='brand-logo'>Title</a>
             <ul className='right hide-on-med-and-down'>
               <li><Link to='/library'>Library</Link></li>
-              <li><Link to='/game'>Game</Link></li>
             </ul>
           </div>
       </nav>

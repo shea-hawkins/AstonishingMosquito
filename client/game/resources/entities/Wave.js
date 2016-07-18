771b09108b9394bd12bf56c21e21ea76700f7c23
Shea Hawkins <Shea Hawkins>
2016-07-18 13:17:35 -0700

    Add example sprite creation

--- client/game/resources/entities/Wave.js
+++ client/game/resources/entities/Wave.js
@@ -3,5 +3,7 @@ import Entity from './Entity';
 export default class Wave extends Entity {
     constructor(store) {
       super(store);
+      this.sprite = new PIXI.Sprite.fromImage('assets/img/Player.png');
+      this.container.addChild(this.sprite);
     }
 }

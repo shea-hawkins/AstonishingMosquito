7416e959dc6c904bb279ef3c6477b5e1be8ea3b0
Shea Hawkins <Shea Hawkins>
2016-07-18 15:10:23 -0700

    Add options parameter to Wave creation

--- client/game/resources/entities/Wave.js
+++ client/game/resources/entities/Wave.js
@@ -1,16 +1,20 @@
 import Entity from './Entity';
 
 export default class Wave extends Entity {
-    constructor(store) {
+    constructor(store, opts) {
       super(store);
+
+      opts = opts || {};
+      // Takes the input value or sets a default/random value
+      this.color = opts.color || Math.floor(0x1000000 * Math.random());
+      this.thickness = opts.thickness || Math.random() * 10;
+      this.radius = opts.radius || 10;
+      this.speed = opts.speed || .05;
+
       this.graphics = new PIXI.Graphics();
-      this.graphics.position.x = 200;
-      this.graphics.position.y = 240;
+      this.graphics.position.x = opts.x || 200;
+      this.graphics.position.y = opts.y || 200;
       this.container.addChild(this.graphics);
-      this.color = 0x7DF9FF;
-      this.thickness = Math.random() * 10;
-      this.radius = 10;
-      this.speed = .05;
     }
     render() {
       this.graphics.clear();

b01599c2ed35321b73c5794ee8e6250cb0216813
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-21 15:38:01 -0700

    Style file upload input

--- client/app/resources/stateless/SongUploadView.js
+++ client/app/resources/stateless/SongUploadView.js
@@ -4,8 +4,8 @@ export default (props) => {
   return (
     <div>
       <form encType="multipart/form-data" onSubmit={props.onSubmit} >
-        <input className="upload-input" type="file" name="song"/>
-        <input type="submit" />
+          <input className="custom-file-upload" type="file" name="song"/>
+        <input className="submit" type="submit" />
       </form>
     </div>
   )

f893d6c1f089f0edea7422d4638e4cfc650cdd85
Shea Hawkins <Shea Hawkins>
2016-07-21 17:32:09 -0700

    Refactor distance calculation out of isColliding

--- client/game/resources/controllers/CollisionDetector.js
+++ client/game/resources/controllers/CollisionDetector.js
@@ -14,26 +14,17 @@ export default class CollisionDetector extends Controller {
       }
     });
   }
-  isColliding(entity, otherEntity) {
+  // this function returns an array of [distance between closest points, distance between furthest points]
+  calculateDistances(entity, otherEntity) {
     var dist = function(p1, p2) {
       return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
     };
     // Assumes that the entity is an instanceof player
     if (otherEntity instanceof Wave) {
       var waveGlobals = otherEntity.graphics.toGlobal(new PIXI.Point(0, 0));
-      var thickness = otherEntity.radius;
-      var radius = otherEntity.radius;
-      // to find the radiius at which the wave is colliding withe the unit
-      // find the distance of the unit from the wave graphic center
-      // in order to keep track of the bounding box, find the distance of the spirite's
-      // closes corner from the center and the distance of the sprite's furthest corner.
-
-      // Sprite's closest corner is the one in which the distance calculation is
-      // smallest.
       var spriteGlobals = entity.sprite.toGlobal(new PIXI.Point(0, 0));
       var width = entity.sprite.width;
       var height = entity.sprite.height;
-      // Needs to determine which diag is the colliding diag.
       var corners = [];
       // top left corner:
       corners.push(new PIXI.Point(
@@ -56,6 +47,8 @@ export default class CollisionDetector extends Controller {
         spriteGlobals.y + height / 2
       ));
 
+      // Sprite's closest corner is the one in which the distance calculation is
+      // smallest.
       var boundingCorners = corners.reduce((mem, corner) => {
         if (dist(corner, waveGlobals) < mem[0]) {
           mem[0] = dist(corner, waveGlobals);
@@ -65,7 +58,21 @@ export default class CollisionDetector extends Controller {
         }
         return mem;
       }, [dist(corners[0], waveGlobals), dist(corners[0], waveGlobals)]);
-      return radius >= boundingCorners[0] && radius <= boundingCorners[1];
+      return boundingCorners;
+    }
+  }
+  isColliding(entity, otherEntity) {
+    if (otherEntity instanceof Wave) {
+      var radius = otherEntity.radius;
+      // to find the radiius at which the wave is colliding withe the unit
+      // find the distance of the unit from the wave graphic center
+      // in order to keep track of the bounding box, find the distance of the spirite's
+      // closes corner from the center and the distance of the sprite's furthest corner.
+
+      // Sprite's closest corner is the one in which the distance calculation is
+      // smallest.
+      var distances = this.calculateDistances(entity, otherEntity);
+      return radius >= distances[0] && radius <= distances[1];
     }
   }
   registerEntitySubject(entity) {

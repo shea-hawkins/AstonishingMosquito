21214f38747750662ba6f807d7c7134ed3ebbc00
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-25 15:39:02 -0700

    Add modal comments

--- client/app/resources/libraryView/LibraryView.js
+++ client/app/resources/libraryView/LibraryView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Motion, spring } from 'react-motion';
 import { connection } from './LibraryModel';
 import { LoadingModal } from '../stateless/Modals';
 import LibraryItemView from '../stateless/LibraryItemView';
@@ -11,23 +10,29 @@ class LibraryView extends React.Component {
     this.props = props;
     this.props.fetchSongList();
   }
+
+  onFormSubmit(e) {
+    this.props.onSubmit(e)
+      .then(() => this.props.fetchSongList());
+  }
+
   render() {
     return (
-            <div className="library-view">
-              <div id="upload">
-                <h2>Upload a Song</h2>
-                <SongUploadView onSubmit={(e) => this.props.onSubmit(e).then(() => this.props.fetchSongList())} />
-              </div>
-              <div id="library-list">
-                <h2>Or Select a Favorite</h2>
-                <ul>
-                  {this.props.songs.map((song, index) => {
-                    return <LibraryItemView song={song} key={song.id} index={index} />;
-                  })}
-                </ul>
-              </div>
-              <LoadingModal show={ this.props.stateName === 'LOADING' } />
-            </div>
+      <div className="library-view">
+        <div id="upload">
+          <h2>Upload a Song</h2>
+          <SongUploadView onSubmit={onFormSubmit.bind(this)} />
+        </div>
+        <div id="library-list">
+          <h2>Or Select a Favorite</h2>
+          <ul>
+            {this.props.songs.map((song, index) => {
+              return <LibraryItemView song={song} key={song.id} index={index} />;
+            })}
+          </ul>
+        </div>
+        <LoadingModal show={ this.props.stateName === 'LOADING' } />
+      </div>
     );
   }
 };

a5b02cf8a1ec24118bbc357d4cebb3ac9106f4a8
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-25 11:57:28 -0700

    Clarify library post route

--- server/resources/library/LibraryRoute.js
+++ server/resources/library/LibraryRoute.js
@@ -6,12 +6,16 @@ var client = redis.createClient();
 var mm = require('musicmetadata'); // Extract metadata from songs
 var md5File = require('md5-file/promise'); // Calculate the hash of song data for redis key
 var multer = require('multer'); // Multi-part form upload parser used to handle the song uploads
-// Directs song upload to be saved in server library folder 
+
+var songLibraryPath = path.join(__dirname + '/../../songLibrary/');
+
+// Directs song upload to be saved in songLibrary folder 
 var upload = multer( { 
-  dest: path.join(__dirname + '/../../songLibrary/')
+  dest: songLibraryPath
 } ).single('song');
 
 var router = express.Router();
+
 
 router.get('/', function(req, res) {
   return new Promise(function(resolve, reject) {
@@ -34,27 +38,39 @@ router.post('/', function (req, res) {
       console.log('multer error', err);
       return;
     }
+    // Get metadata from song file
     new Promise(function(resolve, reject) {
-      mm(fs.createReadStream(path.join(__dirname + '/../../songLibrary/' + req.file.filename)), function (err, metadata) {
+      console.log(fs.createReadStream(songLibraryPath + req.file.filename));
+      mm(fs.createReadStream(songLibraryPath + req.file.filename), function (err, metadata) {
+        if (err) {
+          console.log('music metadata error -', err);
+        }
         delete metadata.picture;
         metadata.fileName = req.file.filename;
+        console.log('metadata saved');
         resolve(metadata);
       });
     })
+    .catch(function(error) {
+      console.log('musicmetadata error', error);
+    })
+    // Add the metadata to the redis database 
     .then(function(metadata) {
-      md5File(req.file.path)
+
+      // calculates file hash for redis key
+      return md5File(req.file.path)
       .then(function(hash) {
         return client.hset('music library', hash, JSON.stringify(metadata));
       }).then(function() {
+        console.log('redis save', metadata);
         res.sendStatus(201);
-      }).catch(function(error) {
-        console.log('database error', error);
-      })
+      });
     })
     .catch(function(error) {
-      console.log('mss error', error);
+        console.log('redis database saving error', error);
     });
   });
 });
+
 
 module.exports = router;

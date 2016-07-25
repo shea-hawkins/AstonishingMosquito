99b68323f929487f5c22f4f8eeff6a611e8d9b9c
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-25 16:17:26 -0700

    Add comments to Entity superclass

--- client/game/resources/entities/Entity.js
+++ client/game/resources/entities/Entity.js
@@ -1,25 +1,27 @@
-// Each entity must have a container property so that it may be
-// added to the stage.
+// Superclass for the Beatbox, Player and Wave entities
 export default class Entity {
-    // clarify the difference between the container
-    // and the things added to the container
-    constructor(store) {
-      this.store = store;
-      // Can be changed to a more reliable unique ID, but we never expect more than ~15
-      // objects on the screen at a time right now.
-      this.id = Math.floor(Math.random() * 10000);
-      this.container = new PIXI.Container();
-      this.renderable = true;
-      store.dispatch({type: 'addEntity', data: this});
-    }
-    destroy() {
-      this.renderable = false;
-      this.store.dispatch({type: 'removeEntity', data: this});
-      this.container.destroy(true);
-    }
-    // can be overridden
-    collide(otherEntity) {}
-    render() {
-      console.error('The render method of each entity must be overridden.');
-    }
+  constructor(store) {
+    this.store = store;
+    // Generate unique ID for each entity
+    this.id = Math.floor(Math.random() * 10000);
+    // Per PIXI standards, each entity must have a container property 
+    // so that it may be added to the stage
+    this.container = new PIXI.Container();
+    this.renderable = true;
+    store.dispatch({type: 'addEntity', data: this});
+  }
+
+  destroy() {
+    this.renderable = false;
+    this.store.dispatch({type: 'removeEntity', data: this});
+    this.container.destroy(true);
+  }
+
+  // Each entity must be collidable and renderable 
+  // These functions can be overridden by the extending class 
+  collide(otherEntity) {}
+  
+  render() {
+    console.error('The render method of each entity must be overridden.');
+  }
 }
